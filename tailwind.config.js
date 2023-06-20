1b939134ec18a228bba6ea2f81d9308d9d5e593f
Your Name <roihansori12@gmail.com>
2023-06-20 18:04:56 +0700

    finish footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const {fontFamily} = require("tailwindcss/defaultTheme")
-
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
@@ -11,14 +10,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        mont: ['var(--font-mont)', ...fontFamily.sans]
+        mont: ["var(--font-mont)", ...fontFamily.sans],
       },
       colors: {
         dark: "#1b1b1b",
         light: "#f5f5f5",
         primary: "#B63E96", // 240,86,199
         primaryDark: "#58E6D9", // 80,230,217
-    }
+      },
+
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
     },
   },
   plugins: [],
